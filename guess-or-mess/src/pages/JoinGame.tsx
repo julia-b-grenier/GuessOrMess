77d52691113bdf83f08833087f60fe5d60298508
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2025-02-01 15:50:46 -0500

    Clean up join game wip

--- guess-or-mess/src/pages/JoinGame.tsx
+++ guess-or-mess/src/pages/JoinGame.tsx
@@ -7,24 +7,6 @@ const JoinGame = () => {
   const storedUsername = Cookies.get('username');
   const navigate = useNavigate();
 
-  const handleInputChange = (e: { target: { name: any; value: any } }) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const validateForm = () => {
-    let isValid = true;
-    const newErrors = { gameCode: "", username: "" };
-
-    // TODO: add game code validation
-
-    // TODO: add username validation
-
-    return isValid;
-  };
 
   const handleJoinGame = () => {
     
@@ -35,35 +17,6 @@ const JoinGame = () => {
       <h2>
         Welcome to the Join Game Page {storedUsername}
       </h2>
-
-      <div>
-        <label>Game Code:</label>
-        <input
-          type="text"
-          name="gameCode"
-          value={formData.gameCode}
-          onChange={handleInputChange}
-          placeholder="Enter game code"
-        />
-        {errors.gameCode && (
-          <p>{errors.gameCode}</p>
-        )}
-      </div>
-
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleInputChange}
-          placeholder="Enter your username"
-        />
-        {errors.username && (
-          <p className="text-red-500 text-sm">{errors.username}</p>
-        )}
-      </div>
-
       <button
         onClick={handleJoinGame}
       >
