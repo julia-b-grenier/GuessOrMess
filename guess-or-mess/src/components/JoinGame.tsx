ca459997f4ae11f18c42427460045aeb46dee092
Tayba <tayba.jusab25@gmail.com>
2025-02-01 11:56:27 -0500

    Setup homepage

---
+++ guess-or-mess/src/components/JoinGame.tsx
@@ -0,0 +1,11 @@
+function JoinGame() {
+    return (
+      <div>
+        <h2>Welcome to Join Game Page</h2>
+        <p>Enter the game code to join!</p>
+      </div>
+    );
+  }
+  
+  export default JoinGame;
+  
---
+++ guess-or-mess/src/components/StartGame.tsx
@@ -0,0 +1,11 @@
+function StartGame() {
+    return (
+      <div>
+        <h2>Welcome to Start Game Page</h2>
+        <p>Game will begin soon!</p>
+      </div>
+    );
+  }
+  
+  export default StartGame;
+  
