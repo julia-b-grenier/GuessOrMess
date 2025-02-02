1c6a68d2e1b845b762391a3724998e449ca351e9
James Luu <jamesluu0917@gmail.com>
2025-02-02 05:18:52 -0500

    fix flashcard

--- guess-or-mess/src/components/FlashCard.tsx
+++ guess-or-mess/src/components/FlashCard.tsx
@@ -24,31 +24,36 @@ const FlashCard: React.FC<FlashCardProps> = ({
   };
 
   return (
-    <div className="border-2 border-black p-4">
-      <h2 className="text-2xl font-mono">{question}</h2>
-      <div className="grid grid-cols-2 gap-4 w-full mt-4">
-        {options.map((option, index) => {
-          let bgColor = "bg-gray-700"; 
-          
-          if (selected === index) {
-            bgColor = "bg-blue-500"; 
-          }
+    <div className="flex justify-center items-center max-w-200 min-w-5 m-auto ">
+      <div className="text-left h-3/4 bg-gradient-to-r from-[#1E6373] to-[#362A64] pt-10 p-3 w-4/5 rounded-2xl min-w-5">
+        <h2 className="text-2xl mb-15 text-white min-w-5">{question}</h2>
+        <div className="flex justify-center items-center min-w-5">
+          <div className="flex grid grid-cols-1 sm:grid-cols-2 gap-4 w-full min-w-5">
+            {options.map((option, index) => {
+              let bgColor = "bg-white opacity-20";
 
-          if (correctIndex !== undefined) {
-            bgColor = index === correctIndex ? "bg-green-500" : "bg-red-500";
-          }
+              if (selected === index) {
+                bgColor = "bg-white opacity-50";
+              }
 
-          return (
-            <button
-              key={index}
-              className={`${bgColor} text-white rounded-lg h-12 shadow-md hover:bg-gray-600`}
-              onClick={() => handleClick(index)}
-              disabled={!clickable}
-            >
-              {option}
-            </button>
-          );
-        })}
+              if (correctIndex !== undefined) {
+                bgColor =
+                  index === correctIndex ? "bg-green-500" : "bg-red-500";
+              }
+
+              return (
+                <button
+                  key={index}
+                  className={`${bgColor} text-black rounded-md h-12 shadow-md hover:opacity-35 m-1 min-w-5`}
+                  onClick={() => handleClick(index)}
+                  disabled={!clickable}
+                >
+                  {option}
+                </button>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
