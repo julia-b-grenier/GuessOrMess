b6522ec51790e52ac87851bcfd6425a94ca9c014
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2025-02-01 22:45:34 -0500

    Fix player waiting room

--- guess-or-mess/src/pages/JoinGame.tsx
+++ guess-or-mess/src/pages/JoinGame.tsx
@@ -1,22 +1,16 @@
 import Cookies from 'js-cookie';  // Make sure to import js-cookie
+import WaitingArea from '../components/WaitingArea';
 
 const JoinGame = () => {
   // Get values from cookies when the component mounts
   const storedUsername = Cookies.get('username');
-  const handleJoinGame = () => {
-    
-  };
-
+  
   return (
     <div>
       <h2>
         Welcome to the Join Game Page {storedUsername}
       </h2>
-      <button
-        onClick={handleJoinGame}
-      >
-        JOIN!
-      </button>
+      <WaitingArea />
     </div>
   );
 };
