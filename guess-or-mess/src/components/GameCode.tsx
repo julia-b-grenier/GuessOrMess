00a95d83b0cde4a341b889622d2a9adfa9f83b59
James Luu <jamesluu0917@gmail.com>
2025-02-02 00:49:11 -0500

    added game code to lobby

--- guess-or-mess/src/components/GameCode.tsx
+++ guess-or-mess/src/components/GameCode.tsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
 import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
+import { blue } from "@mui/material/colors";
+import React from "react";
 
-const GameCode = () => {
+interface GameCodeProps {
+  textToCopy: string;
+}
+
+const GameCode: React.FC<GameCodeProps> = ({ textToCopy }) => {
   const [copied, setCopied] = useState(false);
-  const textToCopy = "game code";
 
   return (
-    <div className="flex items-center space-x-3 border-2 border-black p-3 rounded-lg">
+    <div className="flex items-center space-x-3 p-3 rounded-lg">
       <span className="text-lg font-semibold">{textToCopy}</span>
 
       <CopyToClipboard
@@ -18,11 +23,11 @@ const GameCode = () => {
           setTimeout(() => setCopied(false), 3000); // Reset after 4s
         }}
       >
-        <button className="flex items-center gap-2 bg-white opacity-30 text-white px-3 py-2 rounded-lg hover:opacity-40 transition active:opacity-60 transition">
+        <button className="flex items-center gap-2 bg-[#37373a] text-white px-3 py-2 rounded-lg hover:bg-[#414142] transition active:bg-[#4a4a4a] transition">
           {copied ? (
-            <CheckRoundedIcon sx={{ color: "text.primary" }} />
+            <CheckRoundedIcon color="success" />
           ) : (
-            <ContentCopyRoundedIcon sx={{ color: "text.primary" }} />
+            <ContentCopyRoundedIcon sx={{ color: blue[500] }} />
           )}
         </button>
       </CopyToClipboard>
